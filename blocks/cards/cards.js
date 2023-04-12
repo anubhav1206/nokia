5ee5345694addb349bc342b9d44048ef3cf404ea
Vijay Kumar Jalagari <jalagari@adobe.com>
2023-04-12 18:37:45 +0530

    Cards block styling (#18)

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -5,8 +5,11 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    li.innerHTML = row.innerHTML;
-    [...li.children].forEach((div) => {
+    const a = document.createElement('a');
+    a.innerHTML = row.innerHTML;
+    a.href = row.querySelector('a')?.href;
+    li.prepend(a);
+    [...a.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
